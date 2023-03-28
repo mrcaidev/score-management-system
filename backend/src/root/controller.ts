236f82f68307141fdf932ed1619847512505191b
mrcaidev <mrcaidev@gmail.com>
2023-03-28 20:34:15 +0800

    refactor(backend): remove / endpoint

--- backend/src/root/controller.ts
+++ backend/src/root/controller.ts
@@ -1,13 +1,8 @@
 import { Request, Response } from "express";
 
 export const rootController = {
-  greet,
   checkHealth,
 };
-
-async function greet(_: Request, res: Response) {
-  return res.status(200).json({ data: "成绩管理系统" });
-}
 
 async function checkHealth(_: Request, res: Response) {
   return res.status(200).json({ data: "OK" });
--- backend/src/root/router.ts
+++ backend/src/root/router.ts
@@ -3,6 +3,4 @@ import { rootController } from "./controller";
 
 export const rootRouter: Router = Router();
 
-rootRouter.get("/", rootController.greet);
-
 rootRouter.get("/healthz", rootController.checkHealth);
