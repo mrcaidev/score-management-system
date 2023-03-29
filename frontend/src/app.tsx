2428055609259415268cc4d683426281667a9815
mrcaidev <mrcaidev@gmail.com>
2023-03-29 18:40:35 +0800

    feat(frontend): set up routes

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,6 +1,40 @@
+import { Navigate, Outlet, Route, Router, Routes } from "@solidjs/router";
 import "@unocss/reset/tailwind.css";
 import "virtual:uno.css";
 
 export const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate href="/login" />} />
+        <Route path="login" element={<div>Login</div>} />
+        <Route
+          path="student"
+          element={
+            <div>
+              Student Layout
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path="/" element={<div>Student Home</div>} />
+          <Route path="review" element={<div>Student Review</div>} />
+          <Route path="score" element={<div>Student Score</div>} />
+        </Route>
+        <Route
+          path="teacher"
+          element={
+            <div>
+              Teacher Layout
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path="/" element={<div>Teacher Home</div>} />
+          <Route path="review" element={<div>Teacher Review</div>} />
+          <Route path="score" element={<div>Teacher Score</div>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
