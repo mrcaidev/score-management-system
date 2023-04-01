e969ad3d03025aaa093d854f928c03c5232c0892
mrcaidev <mrcaidev@gmail.com>
2023-04-01 15:17:55 +0800

    refactor(frontend): pass navlinks as props

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -1,9 +1,18 @@
 import { A } from "@solidjs/router";
 import { ThemeToggler } from "components/theme/toggler";
-import { ParentProps } from "solid-js";
+import { IconTypes } from "solid-icons";
+import { For } from "solid-js";
 import { Logout } from "./logout";
 
-export function Sidebar(props: ParentProps) {
+type Props = {
+  nav: {
+    text: string;
+    link: string;
+    icon: IconTypes;
+  }[];
+};
+
+export function Sidebar(props: Props) {
   return (
     <aside class="flex flex-col fixed left-0 top-0 bottom-0 w-90 p-3 bg-gray-200 dark:bg-gray-800">
       <p class="font-bold text-xl">
@@ -12,7 +21,21 @@ export function Sidebar(props: ParentProps) {
         </A>
       </p>
       <hr class="my-3 border-gray-400 dark:border-gray-600" />
-      <nav class="space-y-1 grow overscroll-auto">{props.children}</nav>
+      <nav class="space-y-1 grow overscroll-auto">
+        <For each={props.nav}>
+          {({ text, link, icon: Icon }) => (
+            <A
+              href={link}
+              activeClass="bg-gray-300 dark:bg-gray-700"
+              inactiveClass="hover:bg-gray-300 dark:hover:bg-gray-700"
+              class="flex items-center gap-3 w-full px-4 py-3 rounded-md text-sm"
+            >
+              <Icon size={16} />
+              {text}
+            </A>
+          )}
+        </For>
+      </nav>
       <hr class="mb-3 border-gray-400 dark:border-gray-600" />
       <div class="space-y-1">
         <ThemeToggler />
--- frontend/src/pages/student/layout.tsx
+++ frontend/src/pages/student/layout.tsx
@@ -1,23 +1,26 @@
 import { Outlet } from "@solidjs/router";
 import { RoleGuard } from "components/auth/guards";
-import { NavLink } from "components/nav-link";
 import { Sidebar } from "components/sidebar";
 import { FiFileText, FiSearch } from "solid-icons/fi";
 import { Role } from "utils/types";
 
+const navigation = [
+  {
+    text: "历次成绩",
+    link: "/student/score",
+    icon: FiFileText,
+  },
+  {
+    text: "查分申请",
+    link: "/student/review",
+    icon: FiSearch,
+  },
+];
+
 export default function StudentLayout() {
   return (
     <RoleGuard role={Role.STUDENT}>
-      <Sidebar>
-        <NavLink to="/student/score">
-          <FiFileText size={16} />
-          历次成绩
-        </NavLink>
-        <NavLink to="/student/review">
-          <FiSearch size={16} />
-          查分申请
-        </NavLink>
-      </Sidebar>
+      <Sidebar nav={navigation} />
       <main class="fixed left-90 right-0 top-0 bottom-0">
         <Outlet />
       </main>
