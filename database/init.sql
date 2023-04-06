2d051107bade1f399382dd7151e032bf5ee12134
mrcaidev <mrcaidev@gmail.com>
2023-04-06 20:15:10 +0800

    build: containerize backend and database

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS account_role CASCADE;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-----------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS account_role (
   id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -9,17 +11,7 @@ INSERT INTO account_role (name) VALUES
 ('学生'),
 ('班主任');
 
-ALTER TABLE account_role ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Enable all access to service_role" ON account_role
-AS PERMISSIVE
-FOR ALL
-TO service_role
-USING (true);
-
 -----------------------------------------------------------
-
-DROP TABLE IF EXISTS account CASCADE;
 
 CREATE TABLE IF NOT EXISTS account (
   id TEXT PRIMARY KEY,
@@ -41,17 +33,7 @@ INSERT INTO account (id, name, role, password) VALUES
 ('2020010801010','濮晧宇', 1, CRYPT('2020010801010', GEN_SALT('bf'))),
 ('101', '朱震', 2, CRYPT('101', GEN_SALT('bf')));
 
-ALTER TABLE account ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Enable all access to service_role" ON account
-AS PERMISSIVE
-FOR ALL
-TO service_role
-USING (true);
-
 -----------------------------------------------------------
-
-DROP TABLE IF EXISTS course CASCADE;
 
 CREATE TABLE IF NOT EXISTS course (
   id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -67,20 +49,10 @@ INSERT INTO course (name, max_score) VALUES
 ('化学', 100),
 ('生物', 100);
 
-ALTER TABLE course ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Enable all access to service_role" ON course
-AS PERMISSIVE
-FOR ALL
-TO service_role
-USING (true);
-
 -----------------------------------------------------------
 
-DROP TABLE IF EXISTS exam CASCADE;
-
 CREATE TABLE IF NOT EXISTS exam (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT UNIQUE NOT NULL,
   held_at TIMESTAMPTZ NOT NULL
 );
@@ -89,17 +61,7 @@ INSERT INTO exam (name, held_at) VALUES
 ('2023年3月月考', '2023-03-27 08:00:00'),
 ('2023年4月月考', '2023-04-24 08:00:00');
 
-ALTER TABLE exam ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Enable all access to service_role" ON exam
-AS PERMISSIVE
-FOR ALL
-TO service_role
-USING (true);
-
 -----------------------------------------------------------
-
-DROP TABLE IF EXISTS review_status CASCADE;
 
 CREATE TABLE IF NOT EXISTS review_status (
   id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -113,20 +75,10 @@ INSERT INTO review_status (name) VALUES
 ('已受理'),
 ('已完成');
 
-ALTER TABLE review_status ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Enable all access to service_role" ON review_status
-AS PERMISSIVE
-FOR ALL
-TO service_role
-USING (true);
-
 -----------------------------------------------------------
 
-DROP TABLE IF EXISTS score CASCADE;
-
 CREATE TABLE IF NOT EXISTS score (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   exam_id UUID NOT NULL REFERENCES exam(id),
   course_id SMALLINT NOT NULL REFERENCES course(id),
   student_id TEXT NOT NULL REFERENCES account(id),
@@ -135,17 +87,7 @@ CREATE TABLE IF NOT EXISTS score (
   review_status SMALLINT NOT NULL DEFAULT 1 REFERENCES review_status(id)
 );
 
-ALTER TABLE score ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Enable all access to service_role" ON score
-AS PERMISSIVE
-FOR ALL
-TO service_role
-USING (true);
-
 -----------------------------------------------------------
-
-DROP VIEW IF EXISTS full_score;
 
 CREATE OR REPLACE VIEW full_score AS (
   WITH json_exam AS (
