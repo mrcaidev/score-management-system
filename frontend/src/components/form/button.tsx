42985ca0c2ae3cc1ea48c37c7c9bbbe1c8e93f78
mrcaidev <mrcaidev@gmail.com>
2023-04-08 14:50:09 +0800

    feat(backend): add ghost variant for button

--- frontend/src/components/form/button.tsx
+++ frontend/src/components/form/button.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import { JSX, mergeProps, splitProps } from "solid-js";
 
 type Props = JSX.ButtonHTMLAttributes<HTMLButtonElement> & {
-  color?: "primary" | "success" | "danger";
+  color?: "primary" | "success" | "danger" | "ghost";
   size?: "normal" | "small";
 };
 
@@ -23,18 +23,20 @@ export function Button(props: Props) {
     "children",
   ]);
 
-  const buttonBackground = () => {
+  const colors = () => {
     switch (local.color) {
       case "primary":
-        return "bg-indigo-600 dark:bg-indigo-500 hover:bg-indigo-700 dark:hover:bg-indigo-600";
+        return "bg-indigo-600 dark:bg-indigo-500 hover:bg-indigo-700 dark:hover:bg-indigo-600 text-gray-100";
       case "success":
-        return "bg-green-600 dark:bg-green-500 hover:bg-green-700 dark:hover:bg-green-600";
+        return "bg-green-600 dark:bg-green-500 hover:bg-green-700 dark:hover:bg-green-600 text-gray-100";
       case "danger":
-        return "bg-red-600 dark:bg-red-500 hover:bg-red-700 dark:hover:bg-red-600";
+        return "bg-red-600 dark:bg-red-500 hover:bg-red-700 dark:hover:bg-red-600 text-gray-100";
+      case "ghost":
+        return "bg-gray-300 dark:bg-gray-700 hover:bg-gray-400 dark:hover:bg-gray-600";
     }
   };
 
-  const buttonSize = () => {
+  const sizes = () => {
     switch (local.size) {
       case "normal":
         return "gap-2 px-4 py-3 rounded-md font-bold";
@@ -47,9 +49,9 @@ export function Button(props: Props) {
     <button
       {...rest}
       class={clsx(
-        "flex items-center disabled:bg-gray-600 disabled:hover:bg-gray-600 dark:disabled:bg-gray-400 dark:disabled:hover:bg-gray-400 text-gray-100 transition-colors",
-        buttonBackground(),
-        buttonSize(),
+        "flex items-center disabled:bg-gray-600 disabled:hover:bg-gray-600 dark:disabled:bg-gray-500 dark:disabled:hover:bg-gray-500 transition-colors",
+        colors(),
+        sizes(),
         local.class
       )}
     >
