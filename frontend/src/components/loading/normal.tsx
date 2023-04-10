fbf29a5d83fcb6e7cfbbe22fd0305c5895893471
mrcaidev <mrcaidev@gmail.com>
2023-04-10 13:27:14 +0800

    refactor: remove normal loading component

--- frontend/src/components/loading/normal.tsx
+++
@@ -1,6 +0,0 @@
-import { IconProps } from "solid-icons";
-import { FiLoader } from "solid-icons/fi";
-
-export function Loading(props: IconProps) {
-  return <FiLoader {...props} class="animate-spin" />;
-}
