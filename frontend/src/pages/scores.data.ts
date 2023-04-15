e16435d30c4a842905c46dc731560718ba45a055
mrcaidev <mrcaidev@gmail.com>
2023-04-15 21:59:21 +0800

    perf(frontend): prevent duplicate request on  startup

--- frontend/src/pages/scores.data.ts
+++ frontend/src/pages/scores.data.ts
@@ -4,8 +4,17 @@ import { FullScore } from "utils/types";
 
 export function scoresData() {
   return createResource<FullScore[]>(
-    (_: unknown, { refetching }) =>
-      request.get<FullScore[]>("/scores", { params: refetching }),
+    (_: unknown, { value, refetching }) => {
+      if (value) {
+        const latestExamId = value[0]?.exam.id;
+        const refetchingExamId = (refetching as { examId: string }).examId;
+        if (latestExamId === refetchingExamId) {
+          return value;
+        }
+      }
+
+      return request.get<FullScore[]>("/scores", { params: refetching });
+    },
     { initialValue: [] }
   );
 }
